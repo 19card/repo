e9202c493234c904c13baa029fe2454e81e38387
19card <ad19card@gmail.com>
2019-06-02 21:01:24 -0400

    jelyfish!

---
+++ debs/com.novinha.jellyfishxenhtml_2.0.3_iphoneos-arm/Library/data/var/mobile/Library/SBHTML/NotJellyfish/Config.js
@@ -0,0 +1,21 @@
+/*
+Configurations
+Credits: Original code by Dacal & Junesiphone. Modified by Evelyn. Modified again by Novinha.
+*/
+
+var Weather = true; //Display weather "true" or "false".
+var WeatherColor = "b3e1ff"; // Color is in hexadecimal format.
+
+var Clock = "12h"; //Display time in "12h" or "24h".
+var ClockColor = "ffffff"; // Color is in hexadecimal format.
+
+var Cal = true; //Display day "true" or "false".
+var CalColor = "b3e1ff"; // Color is in hexadecimal format.
+
+var LevelDisplay = true; //Display battery % "true" or "false".
+var LevelDisplayColor = "ffffff"; // Color is in hexadecimal format.
+
+var ShowBattery = true; //Display battery bar "true" or "false".
+var BatteryColor = "ffffff"; // Color is in hexadecimal format.
+
+var Lang = "en"; // Choose language "en", "es", "ca", "fr", "de", "it", "ru", "pl", "pt", "cz", "no", "nl", "fi", "cn", "zh".
